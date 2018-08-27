6516da3f5153f7c5b9b6d2d2dea8747d08c68108
Colby Ford <colbytylerford@gmail.com>
2018-08-27 16:34:23 -0400

    Update test script, remove hello.Rd

--- test/run-test.R
+++ test/run-test.R
@@ -24,15 +24,13 @@ submit(api_url_base,
        output_storage_account_key,
        output_storage_account_container)
 
+list(api_url_base,
+     subscription_key)
+       
+status(api_url_base,
+       subscription_key,
+       workflow_id = NULL)
 
-submit(api_url_base = "https://eastus2.microsoftgenomics.net",
-       subscription_key = "04afabfc1af94c8285faec2f15e4e459",
-       process_args = "R=b37m1",
-       input_storage_account_name = "mygenomicsstorage",
-       input_storage_account_key= "6GyBAbvgw5sqo2xZ1ex5JSda82nu1/edfDvyAcvFSBE9oixUniajPfumB6hfQgs2ArYzt/8I/Sj+F+u9q3B0mA==",
-       input_storage_account_container = "myinputdata",
-       blob_name_1 = "NA12878-chr21_1.fq.gz",
-       blob_name_2 = "NA12878-chr21_2.fq.gz",
-       output_storage_account_name = "mygenomicsstorage",
-       output_storage_account_key = "6GyBAbvgw5sqo2xZ1ex5JSda82nu1/edfDvyAcvFSBE9oixUniajPfumB6hfQgs2ArYzt/8I/Sj+F+u9q3B0mA==",
-       output_storage_account_container = "myoutputdata")
+cancel(api_url_base,
+       subscription_key,
+       workflow_id = NULL)
